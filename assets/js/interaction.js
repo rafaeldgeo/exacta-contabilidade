9af52f81cdf0abf9582bc6c639763761f476fc5a
Rafael <rfldiasapp@gmail.com>
2025-09-09 21:40:37 -0300

    correções de acessibilidade

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -2,21 +2,26 @@
 
 const html = document.querySelector("html");
 const body = document.querySelector("body");
-const divMain = document.querySelector(".main");
-const spanYear = document.querySelector(".credits__year");
 const divContainer = document.querySelector(".container");
+const main = document.querySelector(".main");
 const divModal = document.querySelector(".modal");
 const btnOpen = document.querySelector(".btn");
 const btnClose = document.querySelector(".modal__btn-close");
+const spanYear = document.querySelector(".credits__year");
 
-// get the current Year and place it in footer
+if (!html || !body || !divContainer || !divModal || !main || !btnOpen || !btnClose ) throw new Error ("Some element not found");
+
+// get the current year and place it in the footer
 const currentYear = new Date().getFullYear();
 spanYear.textContent = currentYear;
 
 // open modal
 function openModal() {
+    main.setAttribute("aria-hidden", "true");
     divModal.hidden = false;
-    divModal.querySelector(".modal__content").focus();
+    divModal.setAttribute("aria-modal", "true")
+    trapFocus(divModal);
+    btnClose.focus();
     html.classList.add("no-scroll");
     body.classList.add("no-scroll");
     divContainer.classList.add("container--show-blur");
@@ -24,11 +29,40 @@ function openModal() {
 
 // close modal
 function closeModal() {
+    main.setAttribute("aria-hidden", "false");
     divModal.hidden = true;
+    divModal.removeAttribute("aria-modal");
     btnOpen.focus();
     html.classList.remove("no-scroll");
     body.classList.remove("no-scroll");
     divContainer.classList.remove("container--show-blur");
+}
+
+// focus trap
+function trapFocus(container) {
+    const focusAbleElements = container.querySelectorAll(".modal__btn-close, .link");
+    if (focusAbleElements.length === 0) {
+        console.warn("elements into the modal not found")
+        return;
+    }
+    const firstElement = focusAbleElements[0];
+    const lastElement = focusAbleElements[focusAbleElements.length - 1];
+
+    container.addEventListener("keydown", (e) => {
+        if (e.key === "Tab") {
+            if (e.shiftKey) {
+                if (document.activeElement === firstElement) {
+                    e.preventDefault();
+                    lastElement.focus();
+                }
+            } else {
+                if (document.activeElement === lastElement) {
+                    e.preventDefault();
+                    firstElement.focus();
+                }
+            }
+        }
+    }, {once: true}); 
 }
 
 btnOpen.addEventListener("click", openModal);
@@ -43,9 +77,11 @@ divModal.addEventListener("click", (e) => {
 
 // close the modal when push the Esc key
 addEventListener("keydown", (e) => {
-    if (divModal.hasAttribute("aria-hidden", false) && e.key === "Escape") {
+    if (!divModal.hidden && e.key === "Escape") {
         closeModal();
+
     }
 });
+
 
 
